874c34dbfea76cf0935ed922edd96e8f7ffe38ea
Emre <e.emrecorbaci@gmail.com>
2022-06-14 22:04:06 +0300

    +

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const httpServer = http.createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: '*',
+    origin: 'http://localhost:3000',
     methods: ["GET", "POST"],
   }
 });
